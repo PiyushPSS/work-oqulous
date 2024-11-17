8dc7b499d8e69e7d33a94688dc6783fd61da5127
Piyush <piyushpratapsingh67@gmail.com>
2024-11-17 12:53:51 +0530

    update

---
+++ app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import SearchComponent from './components/SearchComponent';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+//multiple components.
+root.render(<>
+    <SearchComponent />
+</>);
